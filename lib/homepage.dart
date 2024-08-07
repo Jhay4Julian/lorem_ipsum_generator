353f06fbe2c40a000291f10142f17bfa5b4f9f8a
Julian Koripamo <jkoripamo@gmail.com>
2024-08-07 12:03:58 +0100

    api call function

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,8 +11,33 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<String> _paragraphs = [];
+
+  Future<void> fetchLoremText(int paragraphs) async {
+    final response = await http.get(
+      Uri.parse(
+          'https://api.api-ninjas.com/v1/loremipsum?paragraphs=$paragraphs'),
+      headers: {'X-Api-Key': '/ekZbgdfd23YEUx+1IpSUA==dtbKieg0Fc9rtAdg'},
+    );
+    if (response.statusCode == 200) {
+      setState(() {
+        _paragraphs =
+            (json.decode(response.body)['text'] as String).split('\n');
+      });
+    } else {
+      setState(() {
+        _paragraphs = ['Error fetching text'];
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Loren Ipsum Generator'),
+        centerTitle: true,
+      ),
+    );
   }
 }

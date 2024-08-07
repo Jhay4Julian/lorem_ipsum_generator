b53400f6aab2eab8db15e50a88793a980dde9644
Julian Koripamo <jkoripamo@gmail.com>
2024-08-07 12:43:00 +0100

    text field and generate button

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -12,6 +12,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List<String> _paragraphs = [];
+  TextEditingController _controller = TextEditingController();
 
   Future<void> fetchLoremText(int paragraphs) async {
     final response = await http.get(
@@ -37,6 +38,64 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: const Text('Loren Ipsum Generator'),
         centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Text(
+              'Tired of boring Lorem Ipsum?'.toUpperCase(),
+              textAlign: TextAlign.center,
+              style: const TextStyle(fontSize: 20),
+            ),
+            const SizedBox(height: 25),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                const Text(
+                  'Paragraphs:',
+                  style: TextStyle(fontSize: 18),
+                ),
+                // paragraph input
+                SizedBox(
+                  height: 35,
+                  width: 70,
+                  child: TextField(
+                    controller: _controller,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                      contentPadding: EdgeInsets.only(left: 10, right: 10),
+                      filled: true,
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 2,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                // generate button
+                MaterialButton(
+                  color: Colors.green.shade600,
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                  onPressed: () {},
+                  child: const Text(
+                    'Generate',
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.white,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
